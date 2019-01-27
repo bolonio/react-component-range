89dba1785298a285eff8d3113dd566068ffcbe70
Adrián Bolonio <bolonio85@gmail.com>
2019-01-27 13:57:02 +0100

    fixed versions of npm packages, babel and webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,58 @@
+const path = require("path");
+const webpack = require("webpack");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const prod = process.env.NODE_ENV === "production";
+
+module.exports = {
+  mode: prod ? "production" : "development",
+  entry: [path.join(__dirname, "src", "index.js")],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "ReactRange"
+  },
+  externals: {
+    react: {
+      root: "React",
+      amd: "react",
+      commonjs: "react",
+      commonjs2: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      amd: "react-dom",
+      commonjs: "react-dom",
+      commonjs2: "react-dom"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      )
+    }),
+    process.env.ANALYZE_BUNDLE && new BundleAnalyzerPlugin()
+  ].filter(Boolean)
+};
+
+/*
 var path = require("path");
 module.exports = {
   entry: "./src/index.js",
@@ -7,14 +62,6 @@ module.exports = {
     libraryTarget: "commonjs2"
   },
   module: {
-    output: {
-      filename: "[name].js",
-      chunkFilename: "[id].chunk.js",
-      path: path.resolve(__dirname, "build"),
-      publicPath: "/",
-      libraryTarget: "umd",
-      library: "ReactRange"
-    },
     rules: [
       {
         test: /\.js$/,
@@ -49,3 +96,4 @@ module.exports = {
     }
   }
 };
+*/
